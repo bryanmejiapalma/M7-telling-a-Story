18c580b697c2d866e4ad42aa029c5feed349c0f9
bryanmejiapalma <brmejiapalma@s.sfusd.edu>
2025-11-05 13:47:40 -0800

    M7L8(Finished)

--- M07.telling_a_story_workbook/lessons/expressions.gd
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -1,5 +1,16 @@
 extends Control
 
+var bodies := {
+	"sophia": preload("res://assets/pink.png"),
+	"pink": preload("res://assets/pink.png")
+}
+
+var expressions := {
+"emotion_happy": preload("res://assets/emotion_happy.png"),
+"emotion_regular": preload("res://assets/emotion_regular.png"),
+"emotion_sad":preload("res://assets/emotion_sad.png"),
+}
+
 @onready var body: TextureRect = %Body
 @onready var expression: TextureRect = %Expression
 @onready var button_sophia: Button = %ButtonSophia
@@ -7,3 +18,23 @@ extends Control
 @onready var button_regular: Button = %ButtonRegular
 @onready var button_sad: Button = %ButtonSad
 @onready var button_happy: Button = %ButtonHappy
+
+func _ready() -> void:
+	body.texture = bodies["pink"]
+	expression.texture = expressions["happy"]
+
+	button_sophia.pressed.connect(func() -> void:
+		body.texture = bodies["sophia"]
+	)
+	button_pink.pressed.connect(func() -> void:
+		body.texture = bodies["pink"]
+	)
+	button_regular.pressed.connect(func() -> void:
+		expression.texture = expressions["regular"]
+	)
+	button_sad.pressed.connect(func() -> void:
+		expression.texture = expressions["sad"]
+	)
+	button_happy.pressed.connect(func() -> void:
+		expression.texture = expressions["happy"]
+)
